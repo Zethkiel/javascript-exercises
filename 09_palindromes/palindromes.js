90f06ac7d2241cd980093d65314ed3354bb2716c
Zethkiel <marjosephbijer123@gmail.com>
2025-01-04 22:18:24 +0800

    hehehe

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const chars = "abcdefghijklmnopqrstuvwxyz1234567890"
+    const filtered = string.toLowerCase().split('').filter(char => chars.includes(char)).join('');
 
+    const reversed = filtered.split('').reverse().join('');
+    return filtered === reversed;
 };
 
 // Do not edit below this line
